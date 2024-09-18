73a27919c01cadebf826e8ee14daa97a0b065c33
Dhruvi Joshi <dhruvijoshi6@gmail.com>
2024-09-18 14:59:11 +0100

    Language model

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
+model_name = "gpt2"
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+model = GPT2LMHeadModel.from_pretrained(model_name)
 
 app = Flask(__name__)
 
@@ -7,10 +12,26 @@ app = Flask(__name__)
 def index():
     return render_template('main.html')
 
+@app.route("/generate", methods=["GET", "POST"])
+def generate():
+    msg = request.form["msg"]
+    input = msg
+    return generate_text(input)
 
-@app.route("/get", methods=["GET", "POST"])
-def chat():
-    pass
+def generate_text(prompt, max_length=100):
+    input_ids = tokenizer.encode(prompt ,return_tensors="pt")
+    output = model.generate(input_ids, num_return_sequences=1)
+    generated_text = tokenizer.decode(output[0], max_length=max_length ,skip_special_tokens=True)
+    
+    if generated_text.startswith(prompt):
+            generated_text = generated_text[len(prompt):].strip()
+        
+    # Truncate to the last complete sentence
+    sentences = generated_text.split('.')
+    if len(sentences) > 1:
+        generated_text = '.'.join(sentences[:-1]) + '.'
+        
+    return generated_text.strip()
 
 
 if __name__ == '__main__':
